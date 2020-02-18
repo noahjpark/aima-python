149e94bb1b8ab476f39b73d7045aaeebb997454c
Noah Park <noahjinpark@gmail.com>
2020-02-18 13:31:43 -0600

    Update search.py

--- search.py
+++ search.py
@@ -292,7 +292,7 @@ def uniform_cost_search(problem, display=False):
     return best_first_graph_search(problem, lambda node: node.path_cost, display)
 
 
-def depth_limited_search(problem, limit=50):
+def depth_limited_search(problem, limit=10):
     """[Figure 3.17]"""
 
     def recursive_dls(node, problem, limit):
